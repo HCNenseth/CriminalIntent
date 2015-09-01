bb5675a85d59b7e91fd898ede48be42e442bc74c
Hans Christian Nenseth <nenseth@gmail.com>
2015-09-02 01:22:55 +0200

    progress

--- app/src/main/java/com/example/hanschristian/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/example/hanschristian/criminalintent/CrimeListActivity.java
@@ -2,9 +2,6 @@ package com.example.hanschristian.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-/**
- * Created by Hans Christian on 02.09.2015.
- */
 public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
