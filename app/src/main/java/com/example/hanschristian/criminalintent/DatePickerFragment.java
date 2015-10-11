0f14ea0fccf838adaf91c913fb12361b971c00a8
HCNenseth <nenseth@gmail.com>
2015-10-11 13:14:43 +0200

    progress

---
+++ app/src/main/java/com/example/hanschristian/criminalintent/DatePickerFragment.java
@@ -0,0 +1,20 @@
+package com.example.hanschristian.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by HansChristian on 10.10.2015.
+ */
+public class DatePickerFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
