dbc2d23a5438f85f0a229659f04191e0b0146b05
Hans Christian Nenseth <nenseth@gmail.com>
2015-10-11 20:23:21 +0200

    DatePicker

--- app/src/main/java/com/example/hanschristian/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/hanschristian/criminalintent/CrimeFragment.java
@@ -2,6 +2,7 @@ package com.example.hanschristian.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -18,6 +19,7 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -68,7 +70,14 @@ public class CrimeFragment extends Fragment {
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
         mDateButton.setText(DateFormat.format("E, dd MMM yyyy, hh:mm", mCrime.getDate()));
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
@@ -82,5 +91,4 @@ public class CrimeFragment extends Fragment {
 
         return v;
     }
-
 }
